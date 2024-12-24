027f6237a69e2e3dab24ad7728457d37616130ff
Kaoutar-Taki <takikaoutar01@gmail.com>
2024-12-24 16:40:34 +0100

    Add Footer component for improved site structure and navigation

--- portfolioReactTsTailwindCss/src/App.tsx
+++ portfolioReactTsTailwindCss/src/App.tsx
@@ -14,6 +14,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
 import Header from "./components/Header";
 import ScrollTop from "./components/ScrollTop";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -826,61 +827,7 @@ function App() {
         </section>
       </main>
 
-      {/* <!--==================== FOOTER ====================--> */}
-      <footer className="footer">
-        <div className="footer__bg">
-          <div className="footer__container container grid">
-            <div>
-              <h1 className="footer__title">KTAKI</h1>
-              <span className="footer__subtitle">Web Developer</span>
-            </div>
-            <ul className="footer__links">
-              <li>
-                <a href="#services" className="footer__link">
-                  Services
-                </a>
-              </li>
-              <li>
-                <a href="#portfolio" className="footer__link">
-                  Portfolio
-                </a>
-              </li>
-              <li>
-                <a href="#contact" className="footer__link">
-                  Contactme
-                </a>
-              </li>
-            </ul>
-            <div className="footer__socials">
-              <a
-                href="https://www.facebook.com/"
-                target="_blank"
-                className="footer__social"
-              >
-                <i className="uil uil-facebook-f"></i>
-              </a>
-              <a
-                href="https://www.instagram.com/"
-                target="_blank"
-                className="footer__social"
-              >
-                <i className="uil uil-instagram"></i>
-              </a>
-              <a
-                href="https://twitter.com/"
-                target="_blank"
-                className="footer__social"
-              >
-                <i className="uil uil-twitter-alt"></i>
-              </a>
-            </div>
-          </div>
-          <p className="footer__copy">
-            &#169; 2024 Kaoutar Taki. Tous droits réservés.
-          </p>
-        </div>
-      </footer>
-
+      <Footer />
       <ScrollTop />
     </>
   );
