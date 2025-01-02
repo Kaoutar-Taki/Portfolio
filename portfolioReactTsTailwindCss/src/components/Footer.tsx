dc19c6381f2101d9b1c9ffd19c770f612be25705
Kaoutar-Taki <takikaoutar01@gmail.com>
2025-01-02 22:58:24 +0100

    Refactor Footer component for improved styling and accessibility, including French translations and updated social links

--- portfolioReactTsTailwindCss/src/components/Footer.tsx
+++ portfolioReactTsTailwindCss/src/components/Footer.tsx
@@ -3,62 +3,62 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <footer className="pt-8">
-      <div className="bg-hsl(23, 69%, 61%) dark:bg-first-color-second-dark py-8 sm:py-12">
-        <div className="mx-6 sm:mx-4 max-w-full sm:max-w-[768px] md:mx-auto lg:p-0 gap-6 grid sm:grid-cols-2 md:grid-cols-3 md:px-4 lg:px-0">
+      <div className="bg-first-color py-8 sm:py-12 dark:bg-first-color-second-dark">
+        <div className="mx-6 sm:mx-4 max-w-full sm:max-w-4xl md:mx-auto lg:p-0 gap-6 grid sm:grid-cols-2 md:grid-cols-3 md:px-4 lg:px-0">
           <div>
-            <h1 className="text-[1.5rem] mb-1 text-[hsl(23,8%,15%)] font-semibold">
+            <h1 className="text-[1.5rem] mb-1 text-container-color dark:text-title-color-dark font-semibold">
               KTAKI
             </h1>
-            <span className="text-[0.813rem]">Web Developer</span>
+            <span className="text-[0.813rem] text-text-color-light dark:text-text-color-dark">
+              Développeur Web
+            </span>
           </div>
           <ul className="flex flex-col gap-6 sm:flex-row sm:gap-8 list-none">
             <li>
-              <a href="#services" className="hover:text-[#e3b7b7] no-underline">
-                Services
+              <a
+                href="#qualification"
+                className="text-container-color hover:text-first-color-lighter dark:text-text-color-dark dark:hover:text-first-color-lighter no-underline"
+              >
+                Qualification
               </a>
             </li>
             <li>
               <a
-                href="#portfolio"
-                className="hover:text-[#e3b7b7] no-underline"
+                href="#skills"
+                className="text-container-color hover:text-first-color-lighter dark:text-text-color-dark dark:hover:text-first-color-lighter no-underline"
               >
-                Portfolio
+                Compétences
               </a>
             </li>
             <li>
-              <a href="#contact" className="hover:text-[#e3b7b7] no-underline">
-                Contactme
+              <a
+                href="#contact"
+                className="text-container-color hover:text-first-color-lighter dark:text-text-color-dark dark:hover:text-first-color-lighter no-underline"
+              >
+                Contactez-moi
               </a>
             </li>
           </ul>
-          <div className="justify-self-end">
+          <div className="text-container-color dark:text-text-color-dark justify-self-end flex sm:justify-center gap-6">
             <a
-              href="https://www.facebook.com/"
+              href="https://github.com/Kaoutar-Taki"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-[1.25rem] mr-6 hover:text-[#e3b7b7] no-underline"
+              className="text-[1.25rem] hover:text-first-color-lighter dark:hover:text-first-color-lighter no-underline"
             >
-              <i className="uil uil-facebook-f"></i>
+              <i className="uil uil-github"></i>
             </a>
             <a
-              href="https://www.instagram.com/"
+              href="www.linkedin.com/in/kaoutar-taki"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-[1.25rem] mr-6 hover:text-[#e3b7b7] no-underline"
+              className="text-[1.25rem] hover:text-first-color-lighter dark:hover:text-first-color-lighter no-underline"
             >
-              <i className="uil uil-instagram"></i>
-            </a>
-            <a
-              href="https://twitter.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-[1.25rem] mr-6 hover:text-[#e3b7b7] no-underline"
-            >
-              <i className="uil uil-twitter-alt"></i>
+              <i className="uil uil-linkedin"></i>
             </a>
           </div>
         </div>
-        <p className="text-[0.75rem] text-[hsl(23, 8%, 45%)] text-center m-12 sm:mt-18">
+        <p className="text-xs text-center text-text-color-light mt-12 sm:mt-18 dark:text-text-color-dark">
           &#169; 2024 Kaoutar Taki. Tous droits réservés.
         </p>
       </div>
