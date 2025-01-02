37098a239a22d2ecfb0ca7028ac9418f6f269917
Kaoutar-Taki <takikaoutar01@gmail.com>
2025-01-02 22:07:54 +0100

    Update About component with French translations and improved styling

--- portfolioReactTsTailwindCss/src/components/About.tsx
+++ portfolioReactTsTailwindCss/src/components/About.tsx
@@ -1,68 +1,70 @@
 import React from "react";
 import about from "../img/profile.png";
 import pdf from "../pdf/CV_KAOUTAR_TAKI_fr.pdf";
+
 const About: React.FC = () => {
   return (
-    <section className="py-8 md:py-24" id="about">
-      <h2 className="text-xl md:text-4xl text-center text-[hsl(23,8%,15%)] font-semibold">
-        About Me
+    <section className="py-8 md:py-24 dark:bg-[#292623]" id="about">
+      <h2 className="text-xl text-center text-[#292623] font-semibold md:text-4xl dark:text-white">
+        À propos de moi
       </h2>
-      <span className="text-sm md:text-base text-center mb-12 md:mb-16">
-        My introduction
+      <span className="text-sm text-center text-[#746963] block mb-10 md:text-base dark:text-[#b0b0b0]">
+        Mon introduction
       </span>
-      <div className="gap-6 about__container md:grid-cols-2 mx-6 sm:mx-4 max-w-full sm:max-w-[768px] grid md:mx-auto md:gap-20">
-        <img src={about} alt="" className="md:w-[350px] max-w-full h-auto" />
+      <div className="grid gap-6 mx-6 sm:mx-4 max-w-full sm:max-w-[768px] md:grid-cols-2 md:mx-auto md:gap-20">
+        <img
+          src={about}
+          alt="Profil de Kaoutar Taki"
+          className="max-w-full h-auto rounded-full border-4 border-[#e08c57] md:w-[350px] dark:border-[#cb7743]"
+        />
         <div className="about__data">
-          <p className="text-center mb-10 md:text-left">
-            Bonjour, je suis Kaoutar Taki, Développeuse Full Stack passionné par
-            l'art de transformer des lignes de code en expériences interactives
-            et fonctionnelles. Depuis mes premières lignes de HTML, je suis
-            fasciné par le potentiel de la programmation pour créer des
+          <p className="text-center mb-4 text-[#292623]  md:text-left dark:text-[#f0f0f0]">
+            Bonjour, je suis Kaoutar Taki, Développeuse Full Stack passionnée
+            par l'art de transformer des lignes de code en expériences
+            interactives et fonctionnelles. Depuis mes premières lignes de HTML,
+            je suis fascinée par le potentiel de la programmation pour créer des
             solutions innovantes aux défis du numérique.
           </p>
-          <p className="text-center mb-10 md:text-left">
+          <p className="text-center mb-4  text-[#292623] md:text-left dark:text-[#f0f0f0]">
             Je suis constamment à la recherche de nouvelles opportunités pour
             apprendre et grandir en tant que développeuse, et j'ai hâte de
             contribuer à des projets passionnants qui repoussent les limites de
             la technologie.
           </p>
-          <div className="flex justify-evenly mb-10 md:flex md:justify-between">
+          <div className="flex justify-evenly mb-4 md:flex md:justify-between">
             <div>
-              <span className="text-[1.25rem] font-semibold text-[#3b2a2b]">
+              <span className="block text-center text-xl font-semibold text-[#3b2a2b] dark:text-[#e08c57]">
                 01+
               </span>
-              <span className="text-[0.75rem]">
-                Years <br />
-                experience
+              <span className="text-xs block text-center text-[#746963] dark:text-[#b0b0b0]">
+                Années <br /> d'expérience
               </span>
             </div>
             <div>
-              <span className="text-[1.25rem] font-semibold text-[#3b2a2b]">
+              <span className="block text-center text-xl font-semibold text-[#3b2a2b] dark:text-[#e08c57]">
                 03+
               </span>
-              <span className="text-[0.75rem]">
-                Completed <br />
-                project
+              <span className="text-xs block text-center text-[#746963] dark:text-[#b0b0b0]">
+                Projets <br /> réalisés
               </span>
             </div>
             <div>
-              <span className="text-[1.25rem] font-semibold text-[#3b2a2b]">
+              <span className="block text-center text-xl font-semibold text-[#3b2a2b] dark:text-[#e08c57]">
                 02+
               </span>
-              <span className="text-[0.75rem]">
-                Companies <br />
-                worked
+              <span className="text-xs block text-center text-[#746963] dark:text-[#b0b0b0]">
+                Entreprises <br /> où j'ai travaillé
               </span>
             </div>
           </div>
           <div className="flex justify-center md:justify-start">
             <a
-              download=""
+              download="CV_KAOUTAR_TAKI_fr.pdf"
               href={pdf}
-              className="bg-[#db784e] text-white py-4 px-6 rounded-lg font-500 hover:bg-[#ca4f3f] inline-flex items-center no-underline"
+              className="inline-block bg-[#e08c57] text-white py-4 px-6 rounded-lg font-medium hover:bg-[#cb7743] dark:bg-[#cb7743] dark:hover:bg-[#e08c57]"
             >
-              Download CV
-              <i className="uil uil-download-alt text-[1.25rem] ml-2 transition-all"></i>
+              Télécharger le CV
+              <i className="uil uil-download-alt text-xl ml-2 transition-all"></i>
             </a>
           </div>
         </div>
